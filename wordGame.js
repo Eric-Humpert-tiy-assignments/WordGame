e897d49a293cf3ef7efc926a4cb00252974eb872
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-04-10 14:05:59 -0500

    currently buggy commit for this program.  The length checking function works for the longer words but returns undefined for the rest of them.

--- wordGame.js
+++ wordGame.js
@@ -2,31 +2,32 @@
 var wordContainer = document.querySelector('.hiddenWord');
 var selectedWord = '';
 var chosenWord = wordSelector();
+var display = [];
   //word selection function
   function wordSelector() {
 
     return selectedWord += commonWords[Math.floor(Math.random() * commonWords.length)];
 
   }
+  //word hiding function
 
 
-
-console.log(typeof wordContainer);
-
-
-
+  //Word length test function
   function wordLength(str) {
     for (var i = 0; i < str.length; i++) {
       str = chosenWord;
-      if (chosenWord.length < 3) {
-        return selectedWord;
+      if (str.length >= 3) {
+        return str;
       }
     }
   }
 
 
 
-
+//Typechecking console logs
 console.log(chosenWord);
 console.log(typeof chosenWord);
+console.log(typeof wordContainer);
+
+//Output the randomly selected word here
 wordContainer.innerText += wordLength(chosenWord);
